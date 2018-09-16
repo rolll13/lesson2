023eb07bd8b5fa1ad16b5aa8ce741cc512690a62
Roman Mikulich <r.mikulich@mail.ru>
2018-09-16 14:54:13 +0300

    исправления

--- homework.py
+++ homework.py
@@ -1,75 +1,98 @@
 print('Exc_1')
-try:
-    unit = int(input("Что во что переводим: байты в килобайты - 1,"
-                     " килобайты в байты - 2 "))
-except ValueError:
-    print('Неверный выбор, попробуй еще')
-else:
-    if unit != 1 and unit != 2:
+
+
+def task1():
+    try:
+        unit = int(input("Что во что переводим: байты в килобайты - 1,"
+                         " килобайты в байты - 2 "))
+    except ValueError:
         print('Неверный выбор, попробуй еще')
     else:
-        num = int(input('Введите число: '))
-        if unit == 1:
-            print(float(num / 1024))
+        if unit != 1 and unit != 2:
+            print('Неверный выбор, попробуй еще')
         else:
-            print(float(num * 1024))
+            num = int(input('Введите число: '))
+            if unit == 1:
+                return num / 1024
+            else:
+                return num * 1024
+
+
+print(task1())
 
 print("Exc_2")
 
-num = input("Введите число: ")
-mult = 1
-_sum = 0
-for i in num:
-    _sum += int(i)
-    if i != 0:
-        mult *= int(i)
-    else:
-        mult = 0
-print('Сумма: ', _sum, 'Произведение: ', mult)
+
+def task2():
+    number = input("Введите число: ")
+    mult = 1
+    _sum = 0
+    for num in number:
+        _sum += int(num)
+        if num != 0:
+            mult *= int(num)
+        else:
+            mult = 0
+    return 'Сумма: ', _sum, 'Произведение: ', mult
+
+
+print(task2())
 
 print('Exc_3')
 
-min_x = float(input('Минимальное значение x: '))
-max_x = float(input('максимальное значение х: '))
-step = float(input('Шаг: '))
-x = min_x
 
-while x <= max_x:
-    y = -1.24 * x ** 2 + x
-    print('Y = ', y, ' X = ', x)
-    x += step
-else:
-    print('Конец')
+def task3():
+    min_x = float(input('Минимальное значение x: '))
+    max_x = float(input('максимальное значение х: '))
+    step = float(input('Шаг: '))
+    x = min_x
+
+    while x <= max_x:
+        y = -1.24 * x ** 2 + x
+        print('Y = ', y, ' X = ', x)
+        x += step
+    else:
+        return 'Конец'
+
+
+print(task3())
 
 print('Exc_4')
 
-num = input('Введите число: ')
-i = len(num) - 1
-j = 0
-f=True
 
-while j < i:
-    if num[j] == num[i]:
-        j += 1
-        i -= 1
-        f=True
-    else:
-        f = False
-        print('Не палиндром')
-        break
-if f==True:
-    print('Палиндром')
+def task4():
+    num = input('Введите число: ')
+    pos_end = len(num) - 1
+    pos_start = 0
+    palindrome = True
 
+    while pos_start < pos_end:
+        if num[pos_start] == num[pos_end]:
+            pos_start += 1
+            pos_end -= 1
+            palindrome = True
+        else:
+            return 'Не палиндром'
+    if palindrome:
+        return 'Палиндром'
+
+
+print(task4())
 print('Exc_5')
 
-l = [1, -5, 8, 13]
-len_l = len(l)
-_sum = 0
 
-for i in l:
-    if i > 0:
-        _sum += i
-    else:
-        len_l -= 1
-        continue
-print(_sum / len_l)
+def task5():
+    l = [1, -5, 8, 13]
+    len_l = len(l)
+    _sum = 0
+
+    for num in l:
+        if num > 0:
+            _sum += num
+        else:
+            len_l -= 1
+            continue
+    return _sum / len_l
+
+
+print(task5())
